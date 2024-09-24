5ca62f140cc3fbaedc378ce10f39dff7d3b0226a
Sam Barot <sam29061994@gmail.com>
2024-09-24 15:50:01 -0600

    added not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
+import NotFound from "./pages/notFound";
 
 function App() {
   return (
     <div className="min-h-screen bg-background text-foreground">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
